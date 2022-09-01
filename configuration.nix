628d00879dcbee9eee020c66d11758fb2123c421
Blake Spiers <blakespiers93@gmail.com>
2022-09-02 07:16:48 +1000

    Added Fish and Fonts to Alacritty

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,13 @@
     extraGroups = [ "networkmanager" "wheel" ];
   };
 
+  fonts = {
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = ["Hack"];})
+    ];
+    fontDir.enable = true;
+  };
+
   home-manager.users.blake = { pkgs, ... }: {
     nixpkgs.config.allowUnfree = true;
 
@@ -108,13 +115,34 @@
       enable = true;
       userName = "Blake Spiers";
       userEmail = "blakespiers93@gmail.com";
+      extraConfig = {
+        core.editor = "nvim";
+      };
     };
 
     programs.alacritty = {
       enable = true;
       settings = {
+        shell.program = "${pkgs.fish}/bin/fish";
+
         window = {
 	  startup_mode = "Fullscreen";
+	};
+
+	font = {
+	  normal.family = "Hack Nerd Font Mono";
+	  bold.family = "Hack Nerd Font Mono";
+	  italic.family = "Hack Nerd Font Mono";
+	  size = 10.0;
+	  offset = {
+	    x = 0;
+	    y = 0;
+	  };
+	  glyph_offset = {
+	   x = 0;
+	   y = 0;
+	  };
+	  use_thin_strokes = true;
 	};
       };
     };
